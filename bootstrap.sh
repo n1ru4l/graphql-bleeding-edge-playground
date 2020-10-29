8e438d04e48c288f63538743675496121860d9e4
n1ru4l <laurinquast@googlemail.com>
2020-10-29 10:36:46 +0100

    🔧 fix the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 yarn
-cd graphql && yarn && yarn build:npm && cd npmDist && yarn link && cd ../..
+cd graphql-js && yarn && yarn build:npm && cd npmDist && yarn link && cd ../..
 cd express-graphql && yarn && yarn build && cd npmDist && yarn link graphql && yarn link && cd ../..
 yarn link express-graphql
 yarn link graphql
